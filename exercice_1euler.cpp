ce05b82c92816239a16288fceba7631a4935491c
Adrien Pillou <44286944+adrienpillou@users.noreply.github.com>
2018-11-13 14:23:01 +0100

    Add files via upload

--- exercice_1euler.cpp
+++ exercice_1euler.cpp
@@ -15,16 +15,20 @@ int main()
 	cout << "Saisir le seuil de convergence du calcul: ";
 	cin >> seuil;
 	valeur=1./Factorielle(n);
-	cout << "Nombre maximum d'itérations acceptable : ";
+	cout << "Nombre maximum d'iterations acceptable : ";
 	cin >> iter_max ;
+	cout<<"\n";
 	while ((valeur > seuil) && (n < iter_max )){ // Boucle de calcul
 		// Nombre d'itérations inconnu à priori donc borné par le maximum autorisé
 		somme+=valeur;
 		n++;
 		valeur=1./Factorielle(n);
+		cout<<"iteration " <<n<<" : "<<valeur <<endl;
 	}
-	m_euler = sqrt(6*somme);
-	cout <<"Seuil de convergence : "<< seuil <<" Cout (nombre d'itérations) : "<< n << endl;
+	cout<<"\n";
+	//m_euler = sqrt(6*somme);
+	m_euler = 1+somme;
+	cout <<"Seuil de convergence : "<< seuil <<" Cout (nombre d'iterations) : "<< n << endl;
 	cout <<"Valeur approchee de euler = "<< m_euler << " Precision: " << fabs(EULER-m_euler) << endl;
 	system("PAUSE");
 	return(EXIT_SUCCESS);
