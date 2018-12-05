93c8e94f6d225b84bcdf90bcbad80ce33df99a5a
Adrien Pillou <44286944+adrienpillou@users.noreply.github.com>
2018-12-05 16:16:36 +0100

    Add files via upload

--- exercice_7.cpp
+++ exercice_7.cpp
@@ -1,178 +1,190 @@
 using namespace std ;
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
 
-class Entiers
+void AfficherMessage(char*);
+
+
+class Entier
 {
 	unsigned int valeur; // valeur est un attribut privé par défaut
 	public :
-	// méthode externe amicale
-	friend void permut(Entiers &, Entiers &);
-	void saisie() // méthode de saisie
+		
+	friend void Permut(Entier &, Entier &);// méthode externe amicale
+
+	void Saisie() // méthode de saisie
 	{
 		cout << "Valeur entiere a saisir : " ;
 		cin >> valeur;
 	}
 	
-	void affichage(char * chaine) // méthode d'affichage
+	void Affichage(char * chaine) // méthode d'affichage
 	{
 		cout << chaine << valeur << endl;
 	}
 	
-	void addition(Entiers A, Entiers B) // méthode d'addition
+	void Addition(Entier A, Entier B) // méthode d'addition
 	{
 		valeur=A.valeur+B.valeur;
 	}
 	
-	void mult(Entiers A, Entiers B){
+	void Multiplication(Entier A, Entier B){
 		valeur =A.valeur*B.valeur;
-		
 	}
 	
-	void div(Entiers A, Entiers B){		
-		valeur=A.valeur/B.valeur;
+	void Div(Entier A, Entier B){// /!\ div par 0
+		if(B.valeur != 0){
+			valeur=A.valeur/B.valeur;
+		}else{
+			AfficherMessage("Erreur : Division par 0 !");
+			valeur=0;
+		}
 	}
 	
-	void modulo(Entiers A, Entiers B){
-		valeur=A.valeur%B.valeur;
+	void Modulo(Entier A, Entier B){
+		if(B.valeur!=0)valeur=A.valeur%B.valeur;
+		else{
+			AfficherMessage("Erreur : Division par 0 !");
+			valeur=0;
+		}
 	}
-}; // Fin de la classe Entiers
+};// Fin de la classe Entier
 
-class Reels
+class Reel
 {
 	float valeur; // valeur est un attribut privé par défaut
 	public :
 	// méthode externe amicale
-	friend void permut(Reels &, Reels &);
-	void saisie() // méthode de saisie
+
+	friend void Permut(Reel &, Reel &);
+	
+	void Saisie() // méthode de saisie
 	{
 		cout << "Valeur Reel a saisir : " ;
 		cin >> valeur;
 	}
 	
-	void affichage(char * chaine) // méthode d'affichage
+	void Affichage(char * chaine) // méthode d'affichage
 	{
 		cout << chaine << valeur << endl;
 	}
 	
-	void addition(Reels A, Reels B) // méthode d'addition
+	void Addition(Reel A, Reel B) // méthode d'addition
 	{
 		valeur=A.valeur+B.valeur;
 	}
 	
-	void mult(Reels A, Reels B){
+	void Multiplication(Reel A, Reel B){
 		valeur =A.valeur*B.valeur;
 		
 	}
 	
-	void div(Reels A, Reels B){		
-		valeur=A.valeur/B.valeur;
+	void Div(Reel A, Reel B){
+		if(B.valeur!=0)valeur=A.valeur/B.valeur;
+		else AfficherMessage("Erreur : Division par 0 !");	
 	}
 	
-	void modulo(Reels A, Reels B){
-		valeur=A.valeur%B.valeur;
+	void Modulo(Reel A, Reel B){
+		if(B.valeur!=0)valeur=(int)A.valeur%(int)B.valeur;
+		else AfficherMessage("Erreur : Division par 0 !");	
 	}
-};//Fin de la classe des Reels
+};//Fin de la classe des Reel
 
-class Complexes
+class Complexe
 {
 	float Reel; // Reel est un attribut privé par défaut
-	float Im;
+	float Im;// Im est un attribut privé par défaut
 	public :
 	// méthodes externes amicales
-	friend void permut(Complexes &, Complexes &);
-	friend float module(Complexes);
+	friend void Permut(Complexe &, Complexe &);
+	friend float module(Complexe);
 	
-	void saisie(char * chaine) // méthode de saisie
+	void Saisie(char * chaine) // méthode de saisie
 	{
-		cout << chaine << "Partie réelle à saisir : " ;
+		cout << chaine << "Partie reelle a saisir : " ;
 		cin >> Reel;
-		cout << chaine << "Partie imaginaire à saisir : " ;
+		cout << chaine << "Partie imaginaire a saisir : " ;
 		cin >> Im;
 	}
 	
-	void affichage(char * chaine) // méthode d'affichage
+	void Affichage(char * chaine) // méthode d'affichage
 	{
 		cout << chaine << " : " << Reel << "\t" << Im << endl;
 	}
 	
-	void addition(Complexes A, Complexes B) // méthode d'addition
+	void Addition(Complexe A, Complexe B) // méthode d'addition
 	{
 		Reel=A.Reel+B.Reel;
 		Im=A.Im+B.Im;
 	}
-	void conjugue(void) {
+	void Conjugue(void) {
 		Im=-Im;
 	}
-	void multiplication(Complexes A, Complexes B)
+	void Multiplication(Complexe A, Complexe B)
 	{
 		Reel=A.Reel*B.Reel-A.Im*B.Im;
 		Im=A.Reel*B.Im+A.Im*B.Reel;
 	}
 	
-	void division(Complexes A, Complexes B)
+	void Division(Complexe A, Complexe B)
 	{
-		Complexes aux;
-		B.conjugue();
-		aux.multiplication(A,B);
-		aux.division(aux,module(B)*module(B));
+		Complexe aux;
+		B.Conjugue();
+		aux.Multiplication(A,B);
+		//aux.division(aux, Module(B)*Module(B));
 		*this=aux; // similaire à : Reel=aux.Reel; Im=aux.Im;
 	}
-};//Fin de la classe des complexes
+	
+	float Module(Complexe A){
+		return sqrt((A.Reel*A.Reel)+(A.Im*A.Im));
+	}
+};//Fin de la classe des Complexe
 
-//Prototypes des fonctions de permutation pour les différents types de variables
-void permut(Entiers &i, Entiers &j);
-void permut(Reels &i, Reels &j);
-void permut(Complexes &i, Complexes &j);
+
 
 int main()
 {
-	Complexes A,B,C;
-	A.saisie("A ");
-	A.affichage("A");
-	cout << "module(A)=" << module(A) << endl;
-	A.conjugue();
-	A.affichage("conjugué");
-	A.division(A, module(A));
-	A.affichage("A/module(A) = ");
-	B.saisie("B ");
-	C.addition(A,B);
-	C.affichage("somme ");
-	permut(B,C);
-	B.affichage("B");
-	C.affichage("C");
-	A.saisie("A ");
-	A.affichage("A");
-	B.saisie("B ");
-	C.multiplication(A,B);
-	C.affichage("produit ");
-	C.division(A,B);
-	C.affichage("A/B = ");
+	Complexe A,B,C;
+	A.Saisie("A ");
+	A.Affichage("A = ");
+	B.Saisie("B ");
+	C.Addition(A,B);
+	C.Affichage("somme : ");
+	C.Multiplication(A,B);
+	C.Affichage("multiplication : ");
+	C.Division(A,B);
+	C.Affichage("division : ");
+	//C.Modulo(A,B);
+	//C.Affichage("modulo : ");
 	system("PAUSE");
 	return(EXIT_SUCCESS);
 }
 
-void permut(Entiers &i, Entiers &j)
+void Permut(Entier &i, Entier &j)
 {
-	Entiers aux;
+	Entier aux;
 	aux.valeur=i.valeur;
 	i.valeur=j.valeur;
 	j.valeur=aux.valeur;
 }
 
-void permut(Reels &i, Reels &j)
+void Permut(Reel &i, Reel &j)
 {
-	Entiers aux;
+	Entier aux;
 	aux.valeur=i.valeur;
 	i.valeur=j.valeur;
 	j.valeur=aux.valeur;
 }
 
-void permut(Complexes &i, Complexes &j)
+void Permut(Complexe &i, Complexe &j)
 {
-	Entiers aux;
+	Entier aux;
 	aux.valeur=i.valeur;
 	i.valeur=j.valeur;
 	j.valeur=aux.valeur;
 }
 
+void AfficherMessage(char* message){
+	cout<< message <<endl;	
+}
